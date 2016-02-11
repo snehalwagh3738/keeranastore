1464a19709e02d44408f0ca51464aa3ed1822fe6
AK <webonerds@gmail.com>
2016-02-11 14:52:03 +0530

    index commit

--- index.php
+++ index.php
@@ -1,18 +1,2 @@
-<html>
-    <head>
-        <script src="js/jquery-2.2.0.min (1).js" type="text/javascript"></script>
-        <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        
-        
-        <script src="bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
-        
-        
-    </head>
-
-<body>
 
 <h1>Welcome to GIT Repo Keeranastore</h1>
-
-</body>
-
-</html>
