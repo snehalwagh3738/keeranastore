758540c9d3c4d65c10956784c053b943d42598cb
AK <webonerds@gmail.com>
2016-02-11 14:25:27 +0530

    first add to bootstrap css file then next add to jquery file.

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <html>
+    <head>
+        <script src="js/jquery-2.2.0.min (1).js" type="text/javascript"></script>
+        <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        
+        
+        <script src="bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+        
+        
+    </head>
 
 <body>
 
